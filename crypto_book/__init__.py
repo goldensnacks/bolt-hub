3dac0258be349ebcb88e21fc3a20861e98dd5ac2
Jacob Reedijk <jacob@jakedesk>
2025-08-06 20:00:49 +0000

    delete the crypto book we seperated

--- crypto_book/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Crypto Book - A comprehensive crypto trading and analysis toolkit.
-
-This package contains modules for:
-- BTC snapshot and position management
-- Trading dashboards and visualization
-- Risk management and pricing calculations
-"""
